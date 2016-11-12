02a2e2f01917a58bd6f79d6f50f129521fa05182
Aditya Krishnakumar <aditya.kkumar97@gmail.com>
2016-11-12 12:29:53 +0530

    Change #44

---
+++ online-voting-system/admin/add_election.aspx.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace online_voting_system.admin
+{
+    public partial class add_election : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if(Session["login"].ToString() != "admin")
+            {
+                Session.Abandon();
+                Response.Redirect("../default.aspx");
+            }
+            DateValidator.ValueToCompare = DateTime.Now.ToShortDateString();
+        }
+
+        protected void add_elec_btn_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
